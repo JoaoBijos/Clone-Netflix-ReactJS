252125dfe5b87e5489326d8cd0905c8732af0466
JoaoBijos <joaopedrobijos96@gmail.com>
2021-07-20 20:37:39 -0300

    feat03(featured): creating the section with the featured movie or series

--- netflix/src/tmdb.js
+++ netflix/src/tmdb.js
@@ -61,6 +61,26 @@ export default {
                 title: 'Documenários',
                 items: await basicFetch(`/discover/movie?with_geners=99&language=pt-BR&api_key=${API_KEY}`)
             },
-        ]
+        ];
+    },
+
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId){
+            switch(type){
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv': 
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default: 
+                    info = null;
+                break; 
+            }
+        }
+
+        return info
     }
 }
