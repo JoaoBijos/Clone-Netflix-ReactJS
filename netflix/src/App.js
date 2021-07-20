ae34a637a8df82e6cb1c768aaede055ec98fb36d
JoaoBijos <joaopedrobijos96@gmail.com>
2021-07-20 19:30:48 -0300

    feat02(lists): creating the list area

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import tmdb from './tmdb';
+import './App.css';
+
+import MovieRow from './components/MovieRow'
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await tmdb.getHomeList();
+      
+      setMovieList(list);
+    }
+
+    loadAll();
+  },[]);
+
   return(
-    <div>
-      Olá mundo!
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item,key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
